922e85ca48445a9cbaf7c2061f6eb76e95ca17a5
Adam Hurt <92512360+HurtAdam@users.noreply.github.com>
2022-03-17 08:48:04 +0100

    Delete js-folder directory

--- js-folder/script.js
+++
@@ -1,6 +0,0 @@
-const toggle = document.getElementsByClassName("toggle-button")[0];
-const sideBar = document.getElementById("sideBar");
-
-toggle.addEventListener("click", () => {
-    sideBar.classList.toggle('aktivni');
-} )
